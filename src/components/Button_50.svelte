7b7a8e0626f61e9df0f8456c1ef07bbe8427d057
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-02-03 12:34:46 +0300

    Abb block 1

---
+++ src/components/Button_50.svelte
@@ -0,0 +1,14 @@
+<button>
+    Get $50
+</button>
+<style>
+    button {
+        background-color: var(--color-exnova-highlight);
+        color: var(--color-bg-1);
+        font-weight: 500;
+        font-size: 17px;
+        padding: 14px 48px;
+        border-radius: 8px;
+        line-height: 28px;
+    }
+</style>
