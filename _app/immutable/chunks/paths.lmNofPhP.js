bf17275e0897dcde74131abe26833091bfbf0585
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-02-17 16:24:37 +0300

    Add new version

---
+++ _app/immutable/chunks/paths.lmNofPhP.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_lwn1ao)==null?void 0:s.base)??"/exnova-post-video";var a;const o=((a=globalThis.__sveltekit_lwn1ao)==null?void 0:a.assets)??e;export{o as a,e as b};
---
+++ _app/immutable/entry/start.l7eBlIFJ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.pbJi4Upc.js";export{t as start};
