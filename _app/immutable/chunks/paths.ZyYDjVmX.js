61429284460f99d880662e1926aeba9b5370398d
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-03-20 20:33:40 +0300

    New deploy

--- _app/immutable/chunks/paths.ZyYDjVmX.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_7f6m)==null?void 0:s.base)??"/exnova-post-video";var e;const t=((e=globalThis.__sveltekit_7f6m)==null?void 0:e.assets)??a;export{t as a,a as b};
